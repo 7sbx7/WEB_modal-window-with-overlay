af8a5ee4b85ef92fe769dddb0e4bb5f2b30909c0
7sbx7 <sebastianol@outlook.com>
2020-11-20 21:20:08 +0100

    build first verion of project, not responsive at all

--- script.js
+++ script.js
@@ -20,8 +20,13 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 const closeModal = function () {
     modal.classList.add('hidden');
-    overlay.classList.add('hidden')
+    overlay.classList.add('hidden');
 };
 
-btnCloseModal.addEventListener('click', closeModal)
-overlay.addEventListener('click', closeModal)
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+    if (e.key === "Escape") {
+        closeModal();
+    }
+});
